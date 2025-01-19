b14b7a0e219ec56bbbd73af3d5f4cdd569d2133c
Sai Nikhil Patnam <patnamsainikhil@gmail.com>
2025-01-19 02:11:56 -0500

    nav active button added

--- js/dynamicnavbar.js
+++ js/dynamicnavbar.js
@@ -1,5 +1,14 @@
-function dynamicNav() {
-  $(window).scroll(function () {
-    $("nav").toggleClass("scrolled", $(this).scrollTop() > 620);
+$(window).scroll(function () {
+  $("nav").toggleClass("scrolled", $(this).scrollTop() > 620);
+  var scrollPosition = $(this).scrollTop();
+  $(".navbar-nav .nav-link").each(function () {
+    var section = $($(this).attr("href"));
+    if (
+      section.position().top - 120 <= scrollPosition &&
+      section.position().top + section.height() - 120 > scrollPosition
+    ) {
+      $(".navbar-nav .nav-link").removeClass("active");
+      $(this).addClass("active");
+    }
   });
-}
+});
--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,28 +1,19 @@
 $(document).ready(function () {
-  // Add smooth scrolling to all links
   $("a").on("click", function (event) {
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
-
-      // Store hash
       var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
       $("html, body").animate(
         {
-          scrollTop: $(hash).offset().top - 100, // Scroll 100 pixels more
+          scrollTop: $(hash).offset().top - 100,
         },
         800
       );
-
-      // Optionally, update the URL without jumping
       if (history.pushState) {
         history.pushState(null, null, hash);
       } else {
-        window.location.hash = hash; // Fallback for older browsers
+        window.location.hash = hash;
       }
-    } // End if
+    }
   });
 });
